31ac0167dd43348fe7846ecf773f8cba6939d5dd
JAYANTH <jayanthnetha9347@gmail.com>
2024-12-21 15:09:58 +0530

    sitemap2  commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,1 +1,22 @@
- 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MetaPixel from './components/MetaPixel';
+import FreeWorkshop from './pages/FreeWorkshop';
+import About from './components/About';
+
+function App() {
+  return (
+    <>
+      <MetaPixel />
+      <Router>
+        <Routes>
+          <Route path="/" element={<FreeWorkshop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/workshop" element={<FreeWorkshop />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App; 
--- my-app/src/hooks/usePixel.js
+++ my-app/src/hooks/usePixel.js
@@ -1,1 +1,15 @@
- 
+import { useEffect } from 'react';
+
+export const usePixel = () => {
+  const trackCustomEvent = (eventName, data = null) => {
+    if (window.fbq) {
+      if (data) {
+        window.fbq('track', eventName, data);
+      } else {
+        window.fbq('track', eventName);
+      }
+    }
+  };
+
+  return { trackCustomEvent };
+}; 
